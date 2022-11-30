56cb8dd866c46f61a0ba6f193a9618855b7f3a9b
Tilo Thiele <tilo.thiele@emessages.de>
2022-11-30 09:01:06 +0100

    cookie consent

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import type { ReactChild, ReactPortal, ReactNodeArray } from "react";
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
-import { Container, CssBaseline } from '@mui/material';
+import { Container, CssBaseline, Link, ListItemText } from '@mui/material';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { fontStyle } from '@mui/system';
+import CookieConsent from 'react-cookie-consent';
 
 type ReactNode =
   | ReactChild
@@ -42,6 +43,19 @@ const Layout: React.FC<LayoutProps> = ({pageTitle, children}) => {
       <Header/>
       {children}
       <Footer/>
+      <CookieConsent
+        location="bottom"
+        buttonText="Ja, gerne!"
+        enableDeclineButton
+        declineButtonText="Lieber nicht."
+        cookieName="cookie-consent-merker"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        Diese Website verwendet Cookies für ein besseres Surf-Erlebnis. 
+        Siehe <Link href='/dsgvo' underline='none' color='inherit'>Datenschutzerklärung.</Link>
+      </CookieConsent>
     </ThemeProvider>
   )
 }
